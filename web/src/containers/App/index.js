3cfe870ac697eaa711a1d6462fe279ca166d6519
abhishek verma <abhishek.verma@cybrilla.com>
2018-06-27 01:18:28 +0530

    Add router

---
+++ web/src/containers/App/index.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+    );
+  }
+}
+
+export default App;
